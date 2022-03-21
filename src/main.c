71980c4c1527e9a05eb8c36277d062104a0a310d
Yaroslav Apxit3kt0R <666anastyalena@mail.ru>
2022-03-21 09:07:56 +0300

    feature: solve 1 task

--- src/main.c
+++ src/main.c
@@ -1,22 +1,59 @@
-/* hello.c */
+/* main.c */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
 #pragma warning(disable : 4996)
 
 
-int main (void)
+int* input(int* num)
 {
-  char letter;
-  char sentence[200];
-	printf ("Write down the letter: ");
-  scanf("%c", &letter);
-  printf("Sentence :");
-  scanf("%[^\n]%*c", sentence);
+  int i,n;
+  printf("Input number of elements: ");
+  scanf("%d",&n);
+  int * a = malloc(n*sizeof(int));
 
-  printf("ke");
-  printf("%s", sentence);
+  for(i = 0; i < n; i++)
+  {
+    printf("a[%d] = ",i);
+    scanf("%d", &a[i]);
+  }
+  *num = n;
+
+  return a;
+}
+
+bool IsPowerOfFive(int number)
+{
+    for (int x = 1; x <= number; x *= 5)
+    {
+        if (x == number) return true;
+    }
+
+    return false;
+}
+
+void output(int *a, int n)
+{
+  int i;
+
+  for(i = 0; i < n; i++)
+  {
+    if(IsPowerOfFive(a[i])){
+      printf("a[%d] = %d\n",i, a[i]);
+    }
+  }
+}
+
+int main() {
+  int n;
+  int * a = input(&n);
+
+  printf("Printed mass:\n");
+  output(a,n);
+
+  free(a);
 
   return 0;
 }
